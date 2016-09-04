7beec1f429969152db29e7feff40af2518d3ac1a
朱佳杰 <zhujiajie@zhujiajiedemacbook-pro.local>
2016-09-04 15:23:26 +0800

    Initial Commit

---
+++ WFish_xib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WFish_xib
+//
+//  Created by 朱佳杰 on 16/9/4.
+//  Copyright © 2016年 朱佳杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WFish_xib/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WFish_xib
+//
+//  Created by 朱佳杰 on 16/9/4.
+//  Copyright © 2016年 朱佳杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
